e7d3acf581e00cf5a57c45db2cad8561ff35ed7a
mayziran <38189624+mayziran@users.noreply.github.com>
2026-02-28 01:44:14 +0800

    修复

--- Emby.Plugin.StashBox/ExternalIds/FansDBExternalId.cs
+++ Emby.Plugin.StashBox/ExternalIds/FansDBExternalId.cs
@@ -1,13 +1,31 @@
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+
+#if __EMBY__
+#else
+using MediaBrowser.Model.Providers;
+#endif
 
 namespace Emby.Plugin.StashBox.ExternalIds
 {
+#if __EMBY__
     public class FansDBExternalId : IExternalId
+#else
+    public class FansDBExternalId : IExternalId
+#endif
     {
+#if __EMBY__
         public string Name => "FansDB";
+#else
+        public string ProviderName => "FansDB";
+#endif
+
         public string Key => "fansdb";
+
+#if __EMBY__
         public string UrlFormatString => "https://fansdb.cc/{0}";
+#endif
 
         public bool Supports(IHasProviderIds item)
         {
--- Emby.Plugin.StashBox/ExternalIds/PMVStashExternalId.cs
+++ Emby.Plugin.StashBox/ExternalIds/PMVStashExternalId.cs
@@ -1,13 +1,31 @@
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+
+#if __EMBY__
+#else
+using MediaBrowser.Model.Providers;
+#endif
 
 namespace Emby.Plugin.StashBox.ExternalIds
 {
+#if __EMBY__
     public class PMVStashExternalId : IExternalId
+#else
+    public class PMVStashExternalId : IExternalId
+#endif
     {
+#if __EMBY__
         public string Name => "PMVStash";
+#else
+        public string ProviderName => "PMVStash";
+#endif
+
         public string Key => "pmvstash";
+
+#if __EMBY__
         public string UrlFormatString => "https://pmvstash.org/{0}";
+#endif
 
         public bool Supports(IHasProviderIds item)
         {
