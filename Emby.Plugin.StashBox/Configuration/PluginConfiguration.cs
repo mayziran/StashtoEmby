8bc2467fc2645972483260ee89aec09d286fefe1
mayziran <38189624+mayziran@users.noreply.github.com>
2026-02-27 19:15:36 +0800

    ``` refactor(StashBox): 移除过时的GenericEdit依赖并简化插件结构
    移除了对Emby.Web.GenericEdit.dll的依赖，改用MediaBrowser.Model.Plugins中的
    BasePluginConfiguration基类。同时重构了Plugin类，去掉了不必要的HTTP客户端
    和服务注入，简化了插件初始化逻辑。删除了配置页面中不再需要的EditorTitle
    重写方法。
    ```

--- Emby.Plugin.StashBox/Configuration/PluginConfiguration.cs
+++ Emby.Plugin.StashBox/Configuration/PluginConfiguration.cs
@@ -1,8 +1,8 @@
-using Emby.Web.GenericEdit;
+using MediaBrowser.Model.Plugins;
 
 namespace Emby.Plugin.StashBox.Configuration
 {
-    public class PluginConfiguration : EditableOptionsBase
+    public class PluginConfiguration : BasePluginConfiguration
     {
         public PluginConfiguration()
         {
@@ -12,8 +12,6 @@ namespace Emby.Plugin.StashBox.Configuration
             // 预置 5 个已知 Stash-Box 实例
             this.KnownEndpoints = "https://stashdb.org/graphql,https://theporndb.net/graphql,https://fansdb.cc/graphql,https://javstash.org/graphql,https://pmvstash.org/graphql";
         }
-
-        public override string EditorTitle => Plugin.Instance?.Name ?? "StashBox";
 
         /// <summary>
         /// 默认的 Stash-Box endpoint
