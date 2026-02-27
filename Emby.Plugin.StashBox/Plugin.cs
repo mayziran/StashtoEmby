9cdd967b44b6a452de0f47333235c7598d89013b
mayziran <38189624+mayziran@users.noreply.github.com>
2026-02-28 00:34:57 +0800

    修复编译错误

--- Emby.Plugin.StashBox/Plugin.cs
+++ Emby.Plugin.StashBox/Plugin.cs
@@ -2,13 +2,12 @@ using System;
 using MediaBrowser.Common;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Logging;
-using Emby.Plugin.StashBox.Configuration;
 
 [assembly: CLSCompliant(false)]
 
 namespace Emby.Plugin.StashBox
 {
-    public class Plugin : BasePluginSimpleUI<PluginConfiguration>
+    public class Plugin : BasePlugin
     {
         public Plugin(IApplicationHost applicationHost, ILogManager logger)
             : base(applicationHost)
@@ -28,7 +27,5 @@ namespace Emby.Plugin.StashBox
         public override string Name => "StashBox";
 
         public override Guid Id => Guid.Parse("a1b2c3d4-e5f6-7890-abcd-ef1234567890");
-
-        public PluginConfiguration Configuration => this.GetOptions();
     }
 }
